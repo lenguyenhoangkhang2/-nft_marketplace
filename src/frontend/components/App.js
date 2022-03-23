a8b9ed768247682f8a8f3039e5e312ef978c87fb
Le Nguyen Hoang Khang <lenguyenhoangkhang2@gmail.com>
2022-03-23 15:34:58 +0700

    first commit

---
+++ src/frontend/components/App.js
@@ -0,0 +1,103 @@
+import "./App.css";
+import { ethers } from "ethers";
+import { useState } from "react";
+import MarketplaceAbi from "../contractsData/Marketplace.json";
+import MarketplaceAddress from "../contractsData/Marketplace-address.json";
+import NFTAbi from "../contractsData/NFT.json";
+import NFTAddress from "../contractsData/NFT-address.json";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navigation from "./Navbar";
+import Create from "./Create";
+import MyListedItems from "./MyListedItems";
+import MyPurchases from "./MyPurchases";
+import Home from "./Home";
+import { Spinner } from "react-bootstrap";
+
+function App() {
+  const [account, setAccount] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [nft, setNFT] = useState({});
+  const [marketplace, setMarketplace] = useState({});
+
+  const web3Handler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    setAccount(accounts[0]);
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const signer = provider.getSigner();
+
+    loadContracts(signer);
+  };
+
+  const loadContracts = async (signer) => {
+    const marketplace = new ethers.Contract(
+      MarketplaceAddress.address,
+      MarketplaceAbi.abi,
+      signer
+    );
+    setMarketplace(marketplace);
+
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
+    setNFT(nft);
+
+    setLoading(false);
+  };
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navigation web3Handler={web3Handler} account={account} />
+        {loading ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "80vh",
+            }}
+          >
+            <Spinner animation="border" style={{ display: "flex" }} />
+            <p className="mx-3 my-0">Waiting Metamask Connection...</p>
+          </div>
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={<Home marketplace={marketplace} nft={nft} />}
+            />
+            <Route
+              path="/create"
+              element={<Create marketplace={marketplace} nft={nft} />}
+            />
+            <Route
+              path="/my-listed-items"
+              element={
+                <MyListedItems
+                  marketplace={marketplace}
+                  nft={nft}
+                  account={account}
+                />
+              }
+            />
+            <Route
+              path="/my-purchases"
+              element={
+                <MyPurchases
+                  marketplace={marketplace}
+                  nft={nft}
+                  account={account}
+                />
+              }
+            />
+          </Routes>
+        )}
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/frontend/components/MyListedItems.js
@@ -0,0 +1,99 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { Card, Row, Col } from "react-bootstrap";
+
+function MyListedItems({ marketplace, nft, account }) {
+  const [loading, setLoading] = useState(true);
+  const [listedItems, setListedItems] = useState([]);
+  const [soldItems, setSoldItems] = useState([]);
+
+  const loadListedItems = async () => {
+    const itemCount = await marketplace.itemCount();
+    let listedItems = [];
+    let soldItems = [];
+
+    for (let idx = 1; idx <= itemCount; idx++) {
+      const i = await marketplace.items(idx);
+
+      if (i.seller.toLowerCase() === account) {
+        const uri = await nft.tokenURI(i.tokenId);
+        const response = await fetch(uri);
+        const metadata = await response.json();
+        const totalPrice = await marketplace.getTotalPrice(i.itemId);
+
+        let item = {
+          totalPrice,
+          price: i.price,
+          itemId: i.itemId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+        };
+
+        listedItems.push(item);
+        if (i.sold) soldItems.push(item);
+      }
+    }
+
+    setLoading(false);
+    setListedItems(listedItems);
+    setSoldItems(soldItems);
+  };
+
+  useEffect(() => {
+    loadListedItems();
+  }, []);
+
+  if (loading)
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Loading...</h2>
+      </main>
+    );
+
+  return (
+    <div className="flex justify-center">
+      {listedItems.length > 0 ? (
+        <div className="px-5 py-3 container">
+          <h2>Listed</h2>
+          <Row xs={1} md={2} lg={4} className="g-4 py-3">
+            {listedItems.map((item, idx) => (
+              <Col key={idx} className="overflow-hidden">
+                <Card>
+                  <Card.Img variant="top" src={item.image} />
+                  <Card.Footer>
+                    {ethers.utils.formatEther(item.totalPrice)} ETH
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+          {soldItems.length > 0 && (
+            <>
+              <h2>Sold</h2>
+              <Row xs={1} md={2} lg={4} className="g-4 py-3">
+                {soldItems.map((item, idx) => (
+                  <Col key={idx} className="overflow-hidden">
+                    <Card>
+                      <Card.Img variant="top" src={item.image} />
+                      <Card.Footer>
+                        For {ethers.utils.formatEther(item.totalPrice)} ETH -
+                        Recieved {ethers.utils.formatEther(item.price)} ETH
+                      </Card.Footer>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </>
+          )}
+        </div>
+      ) : (
+        <main style={{ padding: "1rem 0" }}>
+          <h2>No Assets</h2>
+        </main>
+      )}
+    </div>
+  );
+}
+
+export default MyListedItems;
